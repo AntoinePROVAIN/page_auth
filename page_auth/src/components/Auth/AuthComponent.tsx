307b47c9af2623849da65470c515d54a9ac0ae5b
antoinePROVAIN <antoineprovain@gmail.com>
2025-05-04 18:09:52 +0200

    fonctionnement tailwindcss pour format responsive

--- page_auth/src/components/Auth/AuthComponent.tsx
+++ page_auth/src/components/Auth/AuthComponent.tsx
@@ -12,23 +12,46 @@ function AuthPage() {
 
     return (
         <>
-            <div className='authComponent_wrapper'>
-                <div className="authComponent_container">
+           <div className='authComponent_wrapper flex flex-col items-center justify-center min-h-screen p-4 md:p-8'>
+                <div className="authComponent_container w-full max-w-md bg-white shadow-md rounded-lg p-6">
                     <div className='authComponent_content'>
-                        <h2 className='auth_title'>Authentification</h2>
+                        <h2 className='auth_title text-center text-2xl font-bold mb-4'>Authentification</h2>
                         <Login />
-                        <div className='or'>ou</div>
-                        <form action="">
-                            <input type='text' id="username" name='username' placeholder='Username' className='auth_input'></input>
-                            <input type='password' id='pwd' name='pwd' placeholder='Password' className='auth_input'></input>
-                            <div className='auth_buttons'>
-                                <input type='button' value="Connexion" className='auth_button'></input>
-                                <input type='button' value="Inscription" className='auth_button' onClick={handleClick}></input>
+                        <div className='or text-center my-4'>ou</div>
+                        <form>
+                            <input
+                                type='text'
+                                id="username"
+                                name='username'
+                                placeholder='Username'
+                                className='auth_input w-full mb-3 p-2 border rounded'
+                            />
+                            <input
+                                type='password'
+                                id='pwd'
+                                name='pwd'
+                                placeholder='Password'
+                                className='auth_input w-full mb-3 p-2 border rounded'
+                            />
+                            <div className='auth_buttons flex flex-col md:flex-row justify-between gap-4'>
+                                <input
+                                    type='button'
+                                    value="Connexion"
+                                    className='auth_button w-full md:w-1/2 py-2 bg-blue-500 text-white rounded'
+                                />
+                                <input
+                                    type='button'
+                                    value="Inscription"
+                                    className='auth_button w-full md:w-1/2 py-2 bg-gray-500 text-white rounded'
+                                    onClick={handleClick}
+                                />
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
+
+
         </>
     )
 }
--- page_auth/src/pages/InscriptionPage.tsx
+++ page_auth/src/pages/InscriptionPage.tsx
@@ -4,6 +4,10 @@ function InscriptionPage() {
     return (
         <> 
             <h1>Inscription</h1>
+            {/* <div className="bg-yellow-300 sm:bg-green-400 md:bg-blue-500 lg:bg-purple-600 text-white p-4">
+                Resize la fenêtre pour voir les changements de couleur !
+            </div> */}
+
         </>
     )
 }
